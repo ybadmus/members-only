f6be80b47e0fac55bf2aaf3117904fdef151b163
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-03-17 13:45:52 -0400

    Edit post controller and model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-  before_action :authenticate_user! only: %i[new create]
+  before_action :authenticate_user!, only: %i[new create]
 
   # GET /posts or /posts.json
   def index
@@ -22,11 +22,11 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { redirect_to root_path, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:post, :text,, :user_id, :integer)
+      params.require(:post).permit(:post, :text, :user_id, :integer)
     end
 end
--- db/migrate/20210317163947_create_posts.rb
+++ db/migrate/20210317163947_create_posts.rb
@@ -1,10 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :post
-      t.string :text,
-      t.string :user_id
-      t.string :integer
+      t.text :post
+      t.integer :user_id
 
       t.timestamps
     end
