c26f5831301a570ff7c880c65c10da886af6b0b6
ybadmus <yusif.badmus@gmail.com>
2021-03-18 05:40:58 +0800

    fix bug with rubocop test that was failing

--- app/views/posts/_post.json.jbuilder
+++ app/views/posts/_post.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! post, :id, :post, :text,, :user_id, :integer, :created_at, :updated_at
+json.extract! post, :id, :post, :user_id, :created_at, :updated_at
 json.url post_url(post, format: :json)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
-require "active_support/core_ext/integer/time"
+# frozen_string_literal: true
+
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -75,5 +77,4 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
 end
